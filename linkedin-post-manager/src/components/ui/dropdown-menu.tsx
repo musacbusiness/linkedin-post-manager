fe6fdbdd41faf0c9298297cad3b9d396d9e6f436
musacbusiness <musacbusiness@users.noreply.github.com>
2026-02-23 10:49:44 -0500

    Remove unused ChevronRight import from dropdown-menu component

--- linkedin-post-manager/src/components/ui/dropdown-menu.tsx
+++ linkedin-post-manager/src/components/ui/dropdown-menu.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { cn } from '@/lib/utils'
-import { Check, ChevronRight } from 'lucide-react'
+import { Check } from 'lucide-react'
 
 const DropdownMenu = DropdownMenuPrimitive.Root
 
